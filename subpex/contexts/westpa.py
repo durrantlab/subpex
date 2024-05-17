4597f4088aef3a3208816b68afd42033254e61bd
Alex Maldonado <alex@oasci.org>
2024-05-16 22:36:55 -0400

    feat: overhaul westpa context manager

--- subpex/contexts/westpa.py
+++ subpex/contexts/westpa.py
@@ -1,100 +1,123 @@
 from typing import Any
 
+import os
 from collections.abc import Iterable, MutableMapping
 
-from .base import BaseContextManager, BaseContextValidator
+import yaml
 
 
-class WestpaContextManager(BaseContextManager):
-    """Contexts for Westpa.
+class System:
+    """System settings for WESTPA."""
 
-    More information on the configuration file can be found
-    [here](https://github.com/westpa/westpa/wiki/Configuration-File).
-    """
-
-    # pylint: disable-next=too-many-statements
-    def __init__(
-        self, yaml_paths: str | Iterable[str] | None = None, **kwargs: dict[str, Any]
-    ) -> None:
-        """
-        Args:
-            yaml_paths: Path(s) to YAML file(s) to load into the context.
-        """
-        self.system_driver: str = "adaptive.System"
-        """The `driver` parameter must be set to a subclass of `WESTSystem`, and given
-        in the form `module.class`.
-        """
-        self.system_module_path: str = "$WEST_SIM_ROOT/adaptive_binning/"
-        """The `module_path` parameter is appended to the system path and indicates
-        where the class is defined.
-        """
-        self.propagation_gen_istates: bool = True
-        """Boolean specifying whether to generate initial states from the basis states.
-        The executable propagator defines a specific configuration block
-        (add internal link to other section), and custom propagators should override
-        the `WESTPropagator.gen_istate()` method.
-        """
-        self.propagation_block_size: int = 1
-        """An integer defining how many segments should be passed to a worker at a time.
-        When using the serial work manager, this value should be set to the maximum
-        number of segments per iteration to avoid significant overhead incurred by the
-        locking mechanism in the WMFutures framework. Parallel work managers might
-        benefit from setting this value greater than one in some instances to
-        decrease network communication load.
-        """
-        self.propagation_save_transition_matrices: bool = True
-        """No description was provided.
-        """
-        self.propagation_max_run_wallclock: str | None = None
-        """A time in `dd:hh:mm:ss` or `hh:mm:ss` specifying the maximum wallclock time
-        of a particular WESTPA run. If running on a batch queuing system, this time
-        should be set to less than the job allocation time to ensure that WESTPA shuts
-        down cleanly.
-        """
-        self.propagation_max_total_iterations: int | None = None
-        """An integer value specifying the number of iterations to run. This parameter is checked against the last completed iteration stored in the HDF5 file, not the number of iterations completed for a specific run. The default value of None only stops upon external termination of the code.
-        """
-        self.data_west_data_file: str = "west.h5"
-        """The name of the main HDF5 data storage file for the WESTPA simulation.
-        """
-        self.data_aux_compression_threshold: int = 1048576
-        """The threshold in bytes for compressing the auxiliary data in a dataset on an iteration-by-iteration basis.
-        """
-        self.data_iter_prec: int = 8
-        """The length of the iteration index with zero-padding. For the default value, iteration 1 would be specified as iter_00000001.
-        """
-        self.data_load_auxdata: bool = False
-        """All datasets without an explicit load: specification are loaded.
-        """
-        self.data_datasets: Iterable[MutableMapping[str, Any]] | None = None
-        """A list of all datasets to load in. Each item must have the following
-        information.
-
-        - `h5path`: Custom path to dataset within the H5 file.
-        - `store`: Store auxdata into H5 File.
-        - `dtype`: Data type of auxdata to be saved.
-        - `scaleoffset`: HDF5 dataset scale offset filter. Defaults to None.
-        - `compression`: HDF5 dataset compression. Defaults to None.
-        - `chunks`: HDF5 dataset chunk size. Defaults to None.
-        """
-        self.data_data_refs_segment: str = (
-            r"$WEST_SIM_ROOT/traj_segs/{segment.n_iter:06d}/{segment.seg_id:06d}"
-        )
-        """TODO:"""
-        self.data_data_refs_basis_state: str = (
-            r"$WEST_SIM_ROOT/bstates/{basis_state.auxref}"
-        )
-        """TODO:"""
-        self.data_data_refs_initial_state: str = (
-            r"$WEST_SIM_ROOT/istates/{initial_state.iter_created}/{initial_state.state_id}.xml"
-        )
-        """TODO:"""
-
-        super().__init__(yaml_paths, **kwargs)
+    def __init__(self) -> None:
+        self.driver: str = "adaptive.System"
+        """The driver parameter must be set to a subclass of `WESTSystem`."""
+        self.module_path: str = "$WEST_SIM_ROOT/adaptive_binning/"
+        """Path where the class is defined."""
 
 
-# pylint: disable-next=too-few-public-methods
-class WestpaContextValidator(BaseContextValidator):
-    """Base class for validating contexts."""
+class Propagation:
+    """Propagation settings for WESTPA."""
 
-    # pylint: disable=unused-argument
+    def __init__(self) -> None:
+        self.gen_istates: bool = True
+        """Generate initial states from basis states."""
+        self.block_size: int = 1
+        """Number of segments passed to a worker at a time."""
+        self.save_transition_matrices: bool = True
+        """Save transition matrices."""
+        self.max_run_wallclock: str | None = None
+        """Max wallclock time for a WESTPA run."""
+        self.max_total_iterations: int | None = None
+        """Max number of iterations to run."""
+
+
+class Data:
+    """Data settings for WESTPA."""
+
+    def __init__(self) -> None:
+        self.west_data_file: str = "west.h5"
+        """Name of the main HDF5 data storage file."""
+        self.aux_compression_threshold: int = 1048576
+        """Threshold in bytes for compressing auxiliary data."""
+        self.iter_prec: int = 8
+        """Length of the iteration index with zero-padding."""
+        self.load_auxdata: bool = False
+        """Load all datasets without an explicit load specification."""
+        self.datasets: Iterable[MutableMapping[str, Any]] | None = None
+        """List of all datasets to load."""
+        self.data_refs: MutableMapping[str, str] = {
+            "segment": r"$WEST_SIM_ROOT/traj_segs/{segment.n_iter:06d}/{segment.seg_id:06d}",
+            "basis_state": r"$WEST_SIM_ROOT/bstates/{basis_state.auxref}",
+            "initial_state": r"$WEST_SIM_ROOT/istates/{initial_state.iter_created}/{initial_state.state_id}.xml",
+        }
+        """References to various data paths."""
+
+
+class Executable:
+    """Executable settings for WESTPA."""
+
+    def __init__(self):
+        self.pre_iteration: MutableMapping[str, Any] = {
+            "enabled": False,
+            "executable": "$WEST_SIM_ROOT/westpa_scripts/pre_iter.sh",
+            "stderr": "$WEST_SIM_ROOT/job_logs/pre_iter.log",
+        }
+        """Pre-iteration executable settings."""
+        self.post_iteration: MutableMapping[str, Any] = {
+            "enabled": True,
+            "executable": "$WEST_SIM_ROOT/westpa_scripts/post_iter.sh",
+            "stderr": "$WEST_SIM_ROOT/job_logs/post_iter.log",
+        }
+        """Post-iteration executable settings."""
+
+
+def to_dict_helper(obj: Any) -> Any:
+    if hasattr(obj, "__dict__"):
+        return {k: to_dict_helper(v) for k, v in obj.__dict__.items()}
+    elif isinstance(obj, list):
+        return [to_dict_helper(v) for v in obj]
+    elif isinstance(obj, dict):
+        return {k: to_dict_helper(v) for k, v in obj.items()}
+    else:
+        return obj
+
+
+class WestpaContextManager:
+
+    def __init__(self, yaml_paths: str | Iterable[str] | None = None, **kwargs: Any):
+        self.system = System()
+        self.propagation = Propagation()
+        self.data = Data()
+        self.executable = Executable()
+
+        if yaml_paths:
+            self.from_yaml(yaml_paths)
+        self.update_config(**kwargs)
+
+    def update_config(self, **kwargs: MutableMapping[str, Any]) -> None:
+        for key, value in kwargs.items():
+            keys = key.split(".")
+            current_level = self
+            for k in keys[:-1]:
+                current_level = getattr(current_level, k)
+            setattr(current_level, keys[-1], value)
+
+    def from_yaml(self, paths: str | Iterable[str]) -> None:
+        if isinstance(paths, str):
+            paths = [paths]
+        for path in paths:
+            with open(path, "r") as f:
+                config = yaml.safe_load(f)
+                self.update_config(**config)
+
+    def get(self):
+        context = to_dict_helper(self)
+        return context
+
+    def to_yaml(self, save_dir: str | None = None) -> None:
+        if save_dir is None:
+            save_dir = ""
+        yaml_path = os.path.join(save_dir, "west.cfg")
+        with open(yaml_path, "w", encoding="utf-8") as f:
+            yaml.dump({"west": self.get()}, stream=f, default_flow_style=False)
